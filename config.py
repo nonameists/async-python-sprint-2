f419b0c0189841cd546236487f237eb98d890214
nonameists <nonameists@gmail.com>
2023-04-20 21:39:12 +0300

    add logger

---
+++ config.py
@@ -0,0 +1,12 @@
+import logging
+
+logger_format = "%(asctime)s - [%(levelname)s] -  %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
+
+logging.basicConfig(
+    filename="logfile.log",
+    filemode="a",
+    format=logger_format,
+    level=logging.INFO
+)
+
+logger = logging.getLogger()
